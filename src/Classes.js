5ddffad9c8b564cb7fe7e6d1f9eebf881ae128aa
ricksy7745 <beckricks@gmail.com>
2024-02-22 16:15:02 -0700

    Classes structure

---
+++ src/Classes.js
@@ -0,0 +1,12 @@
+import { CATEGORIES, SUBCATEGORIES } from './categoriesConfig';
+
+const classes = [
+    {
+        id: 111,
+        course: "CS 111",
+        name: "Intro to Programming",
+        description: "Teaches how to design, develop, reason about, and test programs. Topics include higher-order functions, object-oriented programming, recursion, algorithms, data structures, decomposition, interpreters, and regular expressions.",
+        category: CATEGORIES.FRESHMAN,
+        subcategory: SUBCATEGORIES[CATEGORIES.FRESHMAN].FALL
+    },
+]
