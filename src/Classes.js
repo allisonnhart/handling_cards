8a705f521371e380bf720953ea07860bdc78dcc1
ricksy7745 <beckricks@gmail.com>
2024-02-22 17:15:13 -0700

    Added link attirbute

--- src/Classes.js
+++ src/Classes.js
@@ -7,7 +7,8 @@ export const classes = [
         name: "Intro to Programming",
         description: "Teaches how to design, develop, reason about, and test programs. Topics include higher-order functions, object-oriented programming, recursion, algorithms, data structures, decomposition, interpreters, and regular expressions.",
         category: CATEGORIES.FRESHMAN,
-        subcategory: SUBCATEGORIES[CATEGORIES.FRESHMAN].FALL
+        subcategory: SUBCATEGORIES[CATEGORIES.FRESHMAN].FALL,
+        link: "https://catalog.byu.edu/courses/14243-000"
     },
     {
         id: 202,
